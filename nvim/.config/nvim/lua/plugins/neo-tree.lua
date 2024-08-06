035da84937fb2eff0e58e75e286487ea5dcf108f
NicoGartmann <nico.gartmann97@gmail.com>
2024-08-06 14:35:34 +0200

    Add nvim configuration
    - initialise lazy
    - set some vim options and key-bindings
    - add neo-tree plugin

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
+  },
+  config = function() 
+    require("neo-tree").setup({
+      use_libuv_file_watcher = true,
+      close_if_last_window = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    })
+  end
+}
